cd968a6749d4edd2640b83deb646097d7ae9fbde
andrew0samir <andrewsamir317@gmail.com>
2025-03-21 17:55:57 +0200

    added basecard and header

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <the-header title="RememberMe"></the-header>
+    <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+export default {
+    components: {
+        StoredResources,
+        TheHeader
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official guide to Vue.js',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'The most popular search engine',
+                    link: 'https://google.com'
+                }
+            ]
+        }
+    }
+}
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
