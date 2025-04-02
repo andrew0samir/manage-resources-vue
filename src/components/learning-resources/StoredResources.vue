7d31831ee5448f4cf0f718550901540210b9ab8f
andrew0samir <andrewsamir317@gmail.com>
2025-04-02 22:06:19 +0200

    fix build bug

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import LearningResource from '@/components/learning-resources/LearningResource.vue';
+import LearningResource from './LearningResource.vue';
 
 export default {
     inject: ['resources'],
